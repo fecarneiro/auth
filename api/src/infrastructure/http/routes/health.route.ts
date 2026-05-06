d89c441b79ec0b082e292322f447f7f4be355d79
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-06 16:03:48 -0300

    fix(health): restore health check response and remove error throw

--- api/src/infrastructure/http/routes/health.route.ts
+++ api/src/infrastructure/http/routes/health.route.ts
@@ -4,9 +4,8 @@ import { Router } from 'express';
 export function createHealthCheck() {
   const router = Router();
 
-  router.get('/', (_req: Request, _res: Response) => {
-    throw new Error('Error');
-    // return res.status(200).json({ message: 'Healthcheck OK' });
+  router.get('/', (_req: Request, res: Response) => {
+    return res.status(200).json({ message: 'Healthcheck OK' });
   });
 
   return router;
