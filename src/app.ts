b585cb66a509eaa3a3297b220358c2c429e4668e
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-07 11:21:38 -0300

    chore(repo): rename factories folder to composition

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import { makeLoginUseCase } from './factories/make-login-use-case.js'
-import { makeRegisterUserUseCase } from './factories/make-register-use-case.js'
+import { makeLoginUseCase } from './composition/make-login-use-case.js'
+import { makeRegisterUserUseCase } from './composition/make-register-use-case.js'
 import { errorMiddleware } from './infrastructure/http/middlewares/error.middleware.js'
 import { createAuthRouter } from './infrastructure/http/routes/auth.routes.js'
 import { createHealthCheck } from './infrastructure/http/routes/health.route.js'
