818322eba4ac63aa30a7612f93e09235c66ab5ff
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-11 10:51:20 -0300

    refactor(test): update RedisSessionStore tests to use prefix and ttl

--- src/infrastructure/cache/redis-session-store.test.ts
+++ src/infrastructure/cache/redis-session-store.test.ts
@@ -1,9 +1,9 @@
-import crypto from 'node:crypto'
 import { describe, expect, it, vi } from 'vitest'
 import { type RedisClient, RedisSessionStore } from './redis-session-store.js'
 
 const userId = '019e16fe-4930-7444-a255-9d19fb8afe5a'
-const sessionId = crypto.randomBytes(32).toString('hex')
+const prefix = 'session:'
+const ttl = 3000
 
 function makeSut() {
   const fakeClient: RedisClient = {
@@ -11,23 +11,44 @@ function makeSut() {
     get: vi.fn(),
     del: vi.fn(),
   }
-  const sut = new RedisSessionStore(fakeClient, 1800)
+  const sut = new RedisSessionStore(fakeClient, prefix, ttl)
 
   return { fakeClient, sut }
 }
 
-describe('Redis Session', () => {
-  it('should create a session ID', async () => {
+describe('Redis Session Stpre', () => {
+  it('should set new session', async () => {
     const { sut, fakeClient } = makeSut()
 
     const newSession = await sut.set(userId)
-    console.log(newSession)
 
     expect(newSession).toMatch(/^[a-f0-9]{64}$/)
     expect(fakeClient.setEx).toHaveBeenCalledWith(
-      `session:${newSession}`,
-      1800,
+      prefix + newSession,
+      ttl,
       JSON.stringify({ userId }),
     )
+  })
+
+  it('should get a session', async () => {
+    const { sut, fakeClient } = makeSut()
+
+    vi.mocked(fakeClient.get).mockResolvedValue(JSON.stringify({ userId }))
+
+    const session = await sut.set(userId)
+
+    const data = await sut.get(session)
+
+    expect(data).toBe(userId)
+  })
+
+  it('should invalidate a session', async () => {
+    const { sut, fakeClient } = makeSut()
+
+    const sessionId = '123456'
+
+    await sut.invalidate(sessionId)
+
+    expect(fakeClient.del).toHaveBeenCalledWith(prefix + sessionId)
   })
 })
