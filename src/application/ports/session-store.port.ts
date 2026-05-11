0864f2f910d34511f617e016d964d25dc470477b
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-11 16:19:06 -0300

    refactor(session): update SessionStorePort to use SessionData interface

--- src/application/ports/session-store.port.ts
+++ src/application/ports/session-store.port.ts
@@ -1,8 +1,10 @@
-// Regenerate: new login
-// Invalidate
+export interface SessionData {
+  userId: string
+}
 
 export interface SessionStorePort {
-  set(userId: string): Promise<string>
-  get(sessionId: string): Promise<string | null>
+  set(sessionData: SessionData): Promise<string>
+  get(sessionId: string): Promise<SessionData | null>
   invalidate(sessionId: string): Promise<void>
+  // expire(key: string, seconds: number): Promise<number> //revalidate
 }
