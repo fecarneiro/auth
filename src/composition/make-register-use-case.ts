ad4ce860b51d40f66c1e81f9e662ea1c8eff9648
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-13 10:37:46 -0300

    refactor(register): replace UuidV7IdGenerator with UuidV7EntityIdGenerator

--- src/composition/make-register-use-case.ts
+++ src/composition/make-register-use-case.ts
@@ -1,11 +1,11 @@
 import { RegisterUseCase } from '../application/use-cases/register/register.use-case.js'
 import { BcryptHasher } from '../infrastructure/crypto/bcrypt-hasher.js'
-import { UuidV7IdGenerator } from '../infrastructure/crypto/uuid-v7-id-generator.js'
+import { UuidV7EntityIdGenerator } from '../infrastructure/crypto/uuid-v7-entity-id-generator copy.js'
 import { DrizzleRegisterRepository } from '../infrastructure/database/repository/drizzle-register.repository.js'
 import { DrizzleUserRepository } from '../infrastructure/database/repository/drizzle-user.repository.js'
 
 export function makeRegisterUseCase() {
-  const idGenerator = new UuidV7IdGenerator()
+  const idGenerator = new UuidV7EntityIdGenerator()
   const userRepository = new DrizzleUserRepository()
   const hash = new BcryptHasher()
   const registerRepository = new DrizzleRegisterRepository()
