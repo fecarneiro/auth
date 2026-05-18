057843386333a409ab819d6b999c8c2e56bff4ff
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-18 18:00:11 -0300

    refactor(composition): update OAuth use cases and streamline imports

--- src/infrastructure/composition/composition-root.ts
+++ src/infrastructure/composition/composition-root.ts
@@ -1,8 +1,8 @@
+import { AuthenticateWithOAuthUseCase } from '../../application/use-cases/authenticate-with-oauth/authenticate-with-oauth.use-case.js'
 import { GetAuthenticatedAccountUseCase } from '../../application/use-cases/get-authenticated-account/get-authenticated-account.use-case.js'
-import { LoginWithOAuthUseCase } from '../../application/use-cases/login-with-oauth/login-with-oauth.use-case.js'
+import { LinkOAuthProviderUseCase } from '../../application/use-cases/link-oauth-provider/link-oauth-provider.use-case.js'
 import { LoginWithPasswordUseCase } from '../../application/use-cases/login-with-password/login-with-password.use-case.js'
 import { LogoutUseCase } from '../../application/use-cases/logout/logout.use-case.js'
-import { RegisterWithOAuthUseCase } from '../../application/use-cases/register-with-oauth/register-with-oauth.use-case.js'
 import { RegisterWithPasswordUseCase } from '../../application/use-cases/register-with-password/register-with-password.use-case.js'
 import { BcryptPasswordHasher } from '../crypto/bcrypt-password-hasher.js'
 import { RandomSessionIdGenerator } from '../crypto/random-session-id-generator.js'
@@ -35,19 +35,18 @@ export const compositionRoot = {
     accountRegistrationRepository,
   ),
   logoutUseCase: new LogoutUseCase(sessionStore),
-  loginWithOAuthUseCase: new LoginWithOAuthUseCase(
-    oauthConnectionRepository,
-    accountRepository,
-    sessionIdGenerator,
-    sessionStore,
-  ),
-  registerWithOAuthUseCase: new RegisterWithOAuthUseCase(
+  authenticateWithOAuthUseCase: new AuthenticateWithOAuthUseCase(
     oauthConnectionRepository,
     accountRepository,
     accountRegistrationRepository,
     entityIdGenerator,
     sessionIdGenerator,
     sessionStore,
+  ),
+  linkOAuthProviderUseCase: new LinkOAuthProviderUseCase(
+    oauthConnectionRepository,
+    accountRepository,
+    entityIdGenerator,
   ),
   getAuthenticatedAccountUseCase: new GetAuthenticatedAccountUseCase(
     accountRepository,
